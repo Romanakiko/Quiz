2e36a7da9fb71c70c2d544dc01e7c1554de6acb0
Roman Orlovsky <roman.comm1@gmail.com>
2025-06-10 02:57:53 +0200

    auth button actions

--- src/app/header/user-info/user-info.component.ts
+++ src/app/header/user-info/user-info.component.ts
@@ -1,16 +1,21 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, computed, inject, OnInit, Signal} from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {MatDialog} from '@angular/material/dialog';
 import {AuthComponent} from '../../user/auth/auth.component';
 import {AuthService} from '../../user/auth/auth.service';
 import {UserService} from '../../user/user.service';
+import {MatMenu, MatMenuContent, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
   selector: 'app-user-info',
   imports: [
     MatButton,
-    MatIcon
+    MatIcon,
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem,
+    MatMenuContent
   ],
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.scss'
@@ -23,6 +28,7 @@ export class UserInfoComponent {
 
   isLoggedIn = this.userService.isLoggedIn;
   user = this.userService.userInfo;
+  username: Signal<string | undefined> = computed(() => !this.user()?.name || this.user()?.name === '' ? this.user()?.email : this.user()?.name);
 
   openDialog(): void {
     const dialogRef = this.dialog.open(AuthComponent);
