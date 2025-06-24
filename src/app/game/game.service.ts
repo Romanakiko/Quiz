17a5f9a960c4269a1fcd41b376cca8601ef12a4a
Roman Orlovsky <roman.comm1@gmail.com>
2025-06-24 23:59:40 +0200

    game service added

---
+++ src/app/game/game.service.ts
@@ -0,0 +1,53 @@
+import {inject, Injectable, signal} from '@angular/core';
+import {HeaderService} from '../header/header.service';
+import {SupabaseService} from '../supabase/supabase.service';
+import {UserService} from '../user/user.service';
+import {Folder} from '../folder/folder';
+import {GameStatus, GameTypes} from './game';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  private headerService = inject(HeaderService);
+  private supabaseService = inject(SupabaseService);
+  private userService = inject(UserService);
+
+  constructor() { }
+
+  userFolders = signal<Folder[] | null>(null);
+
+  private async getGames(): Promise<void> {
+    try {
+      this.headerService.loading.set(true);
+      const { data, error } = await this.supabaseService.getOwnRows(this.userService.userInfo()?.email ?? '', 'Game');
+      if (error) throw error;
+
+      this.userFolders.set(
+        data?.map(element => ({
+          id: element.id,
+          name: element.full_name || '',
+          createdAt: new Date(element.created_at),
+          owner_id: element.owner_id,
+          status: element.status as GameStatus,
+          type: element.status as GameTypes,
+          description: element.description,
+          fail_weight: element.fail_weight,
+          timer: element.timer,
+          show_answer_status: element.show_answer_status,
+          show_answers: element.show_answers,
+          standalone: element.standalone,
+        }))
+      );
+      console.log("(FolderService) UserFolders=", this.userFolders());
+    } catch (error) {
+      console.log("FolderError=", error);
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    } finally {
+      this.headerService.loading.set(false);
+    }
+  }
+}
