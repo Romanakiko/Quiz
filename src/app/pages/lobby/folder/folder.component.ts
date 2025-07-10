863357afe980ca97e1acae5b8a18a4f5ba5f73d7
Roman Orlovsky <roman.comm1@gmail.com>
2025-07-10 23:52:07 +0200

    create folder in component

--- src/app/pages/lobby/folder/folder.component.ts
+++ src/app/pages/lobby/folder/folder.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject, linkedSignal, signal} from '@angular/core';
 import {EditableTextComponent} from '../../../ui/editable-text/editable-text.component';
 import {TableComponent} from '../../../ui/table/table.component';
+import {QuestionService} from '../../../question/question.service';
+import {FolderService} from '../../../folder/folder.service';
 
 @Component({
   selector: 'app-folder',
@@ -8,9 +10,22 @@ import {TableComponent} from '../../../ui/table/table.component';
     EditableTextComponent,
     TableComponent
   ],
+  providers: [QuestionService],
   templateUrl: './folder.component.html',
   styleUrl: './folder.component.scss'
 })
 export class FolderComponent {
+  folderService = inject(FolderService);
+  questionService = inject(QuestionService);
 
+  // name = linkedSignal({
+  //   computation: (source, previous) => {
+  //
+  //   }
+  // })
+  name = signal<string>('');
+
+  async createFolder(name: string) {
+    await this.folderService.newFolder(name);
+  }
 }
